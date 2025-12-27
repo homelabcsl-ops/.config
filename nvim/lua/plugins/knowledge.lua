a99a21feb1023a0764f0104d0708c1376f4facb0
homelab <238111483+homelabcsl-ops@users.noreply.github.com>
2025-12-27 03:48:16 +0000

    Auto-sync: 2025-12-27 03:48

--- nvim/lua/plugins/knowledge.lua
+++ nvim/lua/plugins/knowledge.lua
@@ -7,7 +7,6 @@ return {
     ft = "markdown",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      -- "hrsh7th/nvim-cmp", -- Optional: Commented out to prevent dependency errors
       "nvim-telescope/telescope.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
@@ -23,91 +22,90 @@ return {
       { "<leader>od", "<cmd>ObsidianTOC<cr>", desc = "Table of Contents" },
     },
     opts = {
+      -- 1. WORKSPACES (Strict Array)
       workspaces = {
         {
           name = "devops",
           path = "~/obsidian/devops",
         },
-
         {
           name = "personal",
           path = "~/obsidian/personal",
         },
+      }, -- <<< FIXED: Closing brace added here to separate workspaces from global settings
 
-        --- Addition vaults to be addedd here
+      -- 2. GLOBAL SETTINGS (Siblings to workspaces)
+      daily_notes = {
+        folder = "00-Inbox/Daily",
+        date_format = "%Y-%m-%d",
+        template = "daily-note.md",
+      },
 
-        daily_notes = {
-          folder = "00-Inbox/Daily",
-          date_format = "%Y-%m-%d",
-          template = "daily-note.md",
+      completion = {
+        nvim_cmp = false, -- Disabled to fix startup error
+        min_chars = 2,
+      },
+
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
         },
-
-        -- FIX APPLIED HERE
-        completion = {
-          nvim_cmp = false, -- Disabled to fix startup error
-          min_chars = 2,
-        },
-
-        mappings = {
-          ["gf"] = {
-            action = function()
-              return require("obsidian").util.gf_passthrough()
-            end,
-            opts = { noremap = false, expr = true, buffer = true },
-          },
-          ["<cr>"] = {
-            action = function()
-              return require("obsidian").util.smart_action()
-            end,
-            opts = { buffer = true, expr = true },
-          },
-        },
-
-        templates = {
-          subdir = "Templates",
-          date_format = "%Y-%m-%d",
-          time_format = "%H:%M",
-          tags = "",
-          substitutions = {
-            yesterday = function()
-              return os.date("%Y-%m-%d", os.time() - 86400)
-            end,
-          },
-        },
-
-        note_id_func = function(spec)
-          local path = spec.dir / tostring(spec.title)
-          return path:name()
-        end,
-
-        note_frontmatter_func = function(note)
-          local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-            for k, v in pairs(note.metadata) do
-              out[k] = v
-            end
-          end
-          return out
-        end,
-
-        ui = {
-          enable = true,
-          update_debounce = 200,
-          checkboxes = {
-            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-            ["x"] = { char = "", hl_group = "ObsidianDone" },
-          },
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
         },
       },
-      config = function(_, opts)
-        require("obsidian").setup(opts)
-        -- UNIVERSAL FIX: Safety check for templates (Keep this!)
-        local client = require("obsidian").get_client()
-        if client then
-          ---@diagnostic disable-next-line: undefined-field
-          local _ = client.opts.templates and client.opts.templates.subdir or "Templates"
-        end
+
+      templates = {
+        subdir = "Templates",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M",
+        tags = "",
+        substitutions = {
+          yesterday = function()
+            return os.date("%Y-%m-%d", os.time() - 86400)
+          end,
+        },
+      },
+
+      note_id_func = function(spec)
+        local path = spec.dir / tostring(spec.title)
+        return path:name()
       end,
+
+      note_frontmatter_func = function(note)
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+        return out
+      end,
+
+      ui = {
+        enable = true,
+        update_debounce = 200,
+        checkboxes = {
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          ["x"] = { char = "", hl_group = "ObsidianDone" },
+        },
+      },
     },
+
+    config = function(_, opts)
+      require("obsidian").setup(opts)
+      -- UNIVERSAL FIX: Safety check for templates
+      local client = require("obsidian").get_client()
+      if client then
+        ---@diagnostic disable-next-line: undefined-field
+        local _ = client.opts.templates and client.opts.templates.subdir or "Templates"
+      end
+    end,
   },
 }
