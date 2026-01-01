00ada32cb879df6c8b8e2efb13ba8e7d729fc1fb
homelab <238111483+homelabcsl-ops@users.noreply.github.com>
2026-01-01 15:34:58 +0000

    Auto-sync: 2026-01-01 15:34

---
+++ nvim/lua/plugins/training.lua
@@ -0,0 +1,21 @@
+return {
+  -- Speedtyper: In-editor typing practice
+  {
+    "NStefan002/speedtyper.nvim",
+    cmd = "Speedtyper", -- Loads only when you run the command
+    branch = "main",
+    opts = {
+      -- You can customize window transparency and colors here later
+      window = {
+        transparent = true,
+      },
+    },
+  },
+
+  -- Vim-Be-Good: Gamified Vim motion practice
+  {
+    "ThePrimeagen/vim-be-good",
+    cmd = "VimBeGood", -- Loads only when you run the command
+    build = "./install.sh", -- Ensures the necessary binary is built automatically
+  },
+}
