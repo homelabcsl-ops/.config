d52870c27ac9f7cad9c56cc3e1c43ebccf380617
homelab <238111483+homelabcsl-ops@users.noreply.github.com>
2026-01-02 01:17:41 +0000

    Auto-sync: 2026-01-02 01:17

--- nvim/lua/plugins/training.lua
+++ nvim/lua/plugins/training.lua
@@ -16,6 +16,6 @@ return {
   {
     "ThePrimeagen/vim-be-good",
     cmd = "VimBeGood", -- Loads only when you run the command
-    -- REMOVED: build = "./install.sh" (Plugin is now pure Lua)
+    -- NO build line here!
   },
 }
