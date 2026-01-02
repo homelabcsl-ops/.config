d73393fd4b6cb2a7647808991eb58f7a0c1557f9
homelab <238111483+homelabcsl-ops@users.noreply.github.com>
2026-01-02 02:43:23 +0000

    Auto-sync: 2026-01-02 02:43

--- nvim/lua/plugins/training.lua
+++ nvim/lua/plugins/training.lua
@@ -1,12 +1,21 @@
 return {
+  -- Speedtyper: In-editor typing practice
   {
     "NStefan002/speedtyper.nvim",
+    branch = "main",
     cmd = "Speedtyper",
-    opts = { window = { transparent = true } },
+    config = function()
+      -- Execute settings commands only after the plugin loads
+      -- 1. Disable sound (Fixes "No tools" error)
+      vim.cmd("SpeedtyperSettings sound enabled false")
+      -- 2. Set transparency (Optional)
+      vim.cmd("SpeedtyperSettings window transparent true")
+    end,
   },
+
+  -- Vim-Be-Good: Gamified Vim motion practice
   {
     "ThePrimeagen/vim-be-good",
     cmd = "VimBeGood",
-    -- NO build line here!
   },
 }
