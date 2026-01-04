4434c9de68a05c7b5fbff28d3386daa1bed02ef3
homelab <238111483+homelabcsl-ops@users.noreply.github.com>
2026-01-04 03:01:11 +0000

    Auto-sync: 2026-01-04 03:01

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keymaps her
+
+-- Map <Space>H to Search Help (Telescope)
+vim.keymap.set("n", "<leader>H", "<cmd>Telescope help_tags<cr>", { desc = "Search Help" })
